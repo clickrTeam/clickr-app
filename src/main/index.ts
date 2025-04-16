0ad31e9a7d96893dd6afbc8cccb9e3855b1dc35d
Ryan Dalrymple <ryan.d235711@gmail.com>
2025-04-16 13:09:32 -0600

    started to debug but having issues. window still won't start and problem doesn't appear to be in preload script or app.tsx

--- src/main/index.ts
+++ src/main/index.ts
@@ -140,7 +140,7 @@ function createWindow(): void {
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.whenReady().then(() => {
+app.whenReady().then((createWindow) => {
   // Set app user model id for windows
   electronApp.setAppUserModelId('com.electron')
 
