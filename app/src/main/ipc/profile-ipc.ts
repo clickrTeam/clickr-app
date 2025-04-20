3fd25f3bc94e0b1d48328a50a0e07cfda6080e67
TimB44 <u1414110@umail.utah.edu>
2025-04-20 14:21:13 -0600

    Move IPC handlers to its own file

---
+++ app/src/main/ipc/profile-ipc.ts
@@ -0,0 +1,28 @@
+import { ipcMain } from 'electron'
+import { profileStore } from '../services/profile-store'
+import { Profile } from '../../models/Profile'
+import { sendActiveProfile } from '../services/daemon-bridge'
+
+export function registerProfileHandlers() {
+  ipcMain.handle('get-profiles', () => {
+    return profileStore.getProfiles().map(p => p.toJSON())
+  })
+
+  ipcMain.handle('get-active-profile', () => {
+    return profileStore.getActive()
+  })
+
+  ipcMain.handle('create-profile', (_event, name: string) => {
+    return profileStore.create(name).toJSON()
+  })
+
+  ipcMain.handle('set-active-profile', (_event, index: number) => {
+    profileStore.setActiveByIndex(index)
+    sendActiveProfile(profileStore.getProfiles()[index])
+  })
+
+  ipcMain.handle('update-profile', (_event, index: number, profileData) => {
+    const updated = Profile.fromJSON(profileData)
+    profileStore.setProfileByIndex(index, updated)
+  })
+}
