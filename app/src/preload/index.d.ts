1eec5ea956d235de6741c788dce05ce5c4da39f0
TimB44 <u1414110@umail.utah.edu>
2025-04-20 14:21:38 -0600

    Update preload to expose new IPC API

--- app/src/preload/index.d.ts
+++ app/src/preload/index.d.ts
@@ -1,8 +1,16 @@
 import { ElectronAPI } from '@electron-toolkit/preload'
+import { Profile } from "../models/Profile.ts"
+export interface API {
+  getProfiles(): Promise<Profile[]>;
+  getActiveProfile(): Promise<number | null>;
+  createProfile(name: string): Promise<void>;
+  setActiveProfile(index: number): Promise<void>;
+  updateProfile(index: number, profileData: Profile): Promise<void>;
+}
+
 
 declare global {
   interface Window {
-    electron: ElectronAPI
-    api: unknown
+    api: API
   }
 }
