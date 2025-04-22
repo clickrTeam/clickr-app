dd72dbec137590859fc384b4ad3314c613a606e5
TimB44 <u1414110@umail.utah.edu>
2025-04-21 22:19:59 -0600

    Add KeySelector component

---
+++ app/src/renderer/src/components/KeySelector.tsx
@@ -0,0 +1,29 @@
+import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "./ui/select"
+import { keys } from "src/models/Keys"
+
+/**
+ * Props for KeySelecter:
+ * - selectedKey: currently selected key (optional)
+ * - onSelect: callback when user picks a key
+ */
+type KeySelecterProps = {
+  selectedKey?: string
+  onSelect: (value: string) => void
+}
+
+export default function KeySelecter({ selectedKey, onSelect }: KeySelecterProps) {
+  return (
+    <Select value={selectedKey} onValueChange={onSelect}>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Select key" />
+      </SelectTrigger>
+      <SelectContent>
+        {keys.map((key) => (
+          <SelectItem key={key} value={key}>
+            {key}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
+  )
+}
