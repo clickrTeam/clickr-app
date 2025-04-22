21e085abb559e7d0d5c5043fceff06358c29598f
bodePacker <bjackpacker@gmail.com>
2025-04-22 15:11:07 -0600

    Pointed app to hosted DB. REVERT LATER

--- app/src/main/ipc/api-ipc.ts
+++ app/src/main/ipc/api-ipc.ts
@@ -1,7 +1,8 @@
 import { ipcMain } from 'electron'
 import axios from 'axios'
 
-const BASE_URL = 'http://127.0.0.1:8000/api/'
+//const BASE_URL = 'http://127.0.0.1:8000/api/'
+const BASE_URL = 'https://temp-django-docker-production.up.railway.app/api/'
 
 const api = axios.create({
   baseURL: BASE_URL,
