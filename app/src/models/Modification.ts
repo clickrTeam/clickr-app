999f604ba0aef9a05dfa4dec1d5df14c69d94aa0
Timothy Blamires <u1414110@umail.utah.edu>
2025-10-13 19:53:39 -0600

    Add modification class

--- app/src/models/Modification.ts
+++ app/src/models/Modification.ts
@@ -1,5 +1,6 @@
 import { Trigger } from "./Trigger"
 import { Bind } from "./Bind"
+import { LLRemapping } from "./LowLevelProfile";
 export enum ModType {
   Advanced = 'advanced'
 }
@@ -7,7 +8,6 @@ export enum ModType {
 //TOOD: Should this be an abstract class or just an interface
 export abstract class Modification {
   abstract toJSON(): object
-  abstract toLL(): LLMod
 }
 
 export class AdvancedModificaiton extends Modification {
@@ -26,18 +26,25 @@ export class AdvancedModificaiton extends Modification {
       bind: this.bind
     }
   }
-  toLL(): LLMod {
-    return {
-      from: this.trigger.toLL(),
-      to: this.bind.toLL(),
-      //todo: how should this be set
-      priority: 1
-    };
+  toLL(): LLRemapping {
+    let ll_trigger = this.trigger.toLL();
+    let ll_bind = this.bind.toLL();
+    if ("triggers" in ll_trigger) {
+      return {
+        ...ll_trigger,
+        binds: ll_bind
+      }
+
+    } else {
+      return {
+        trigger: ll_trigger,
+        binds: ll_bind,
+      }
+    }
   }
+
 
   static fromJSON(o: object): AdvancedModificaiton {
     throw new Error("")
-
   }
-
 }
