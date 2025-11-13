7bd82e08296f76e572e3fdec44fbcf1821b292ec
Ryan Dalrymple <ryan.d235711@gmail.com>
2025-11-13 14:12:37 -0700

    added first test and test never finishes

--- app/tests/ll_shortcuts.test.ts
+++ app/tests/ll_shortcuts.test.ts
@@ -7,3 +7,17 @@ import * as utils from './test_utils/test_utils'
 import { MacKey, WinKey, LinuxKey, Letters, Digits, Function } from '../src/models/Keys'
 import * as T from '../src/models/Trigger'
 import * as B from '../src/models/Bind'
+import * as LL from '../src/models/LowLevelProfile'
+import log from 'electron-log'
+
+/**
+ * Tests that ensure basic shortcuts are represented in their low level form
+ */
+describe('Basic Low Level Shortcut', () => {
+  test('Basic Windows Shortcut', () => {
+    const prof = utils.generateSmallShortcutProfileWindows()
+    const ll_prof = prof.toLL()
+    log.info('ll_prof: ', ll_prof)
+    expect(2 + 2).toBe(4)
+  })
+})
