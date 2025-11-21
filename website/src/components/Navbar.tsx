98b815577e28450e2b59f477a64601e77875131a
Bode Packer <bjackpacker@gmail.com>
2025-11-20 18:38:05 -0700

    added on hover for clickr web

--- website/src/components/Navbar.tsx
+++ website/src/components/Navbar.tsx
@@ -119,7 +119,7 @@ const Navbar = () => {
     >
       <div className="container mx-auto px-4 flex items-center justify-between">
         <Link to="/" className="flex items-center">
-          <span className="text-2xl font-bold text-foreground">Clickr</span>
+          <span className="text-2xl font-bold text-foreground hover:text-clickr-blue">Clickr</span>
         </Link>
 
         {/* Desktop Navigation */}
