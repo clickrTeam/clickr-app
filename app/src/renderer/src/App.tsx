855e337db4ae4a104d74779c6c5fea6ce6cbc292
Luke Hamling <122407541+Lukehamling@users.noreply.github.com>
2026-01-05 16:21:21 -0700

    fx scrollbar on navbar.

--- app/src/renderer/src/App.tsx
+++ app/src/renderer/src/App.tsx
@@ -67,7 +67,7 @@ function App(): JSX.Element {
       <Navbar isAuthenticated={isAuthenticated} username={username} logout={logout} />
 
       {/* Add padding-top to account for fixed navbar */}
-      <div className="w-full px-4 flex-grow py-8 pt-20">
+      <div className="w-full px-4 flex-grow py-8 pt-20 overflow-y-auto max-h-screen">
         <Routes>
           <Route
             path="/"
